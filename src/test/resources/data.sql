8b883215aefb7ada2c30d329731a0b5acb225084
Mladen Čikara <mladen.cikara@gmail.com>
2020-05-13 18:04:22 +0200

    Jednostavni primjer sa tri tablice koje imaju liste u sebi

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -8,3 +8,28 @@ INSERT INTO post_details (post_id, created_on, created_by) VALUES
  (1, CURRENT_DATE, 'Author 1')
 ,(2, CURRENT_DATE, 'Author 2')
 ;
+
+
+INSERT INTO atable(id) VALUES
+ (1)
+,(2)
+,(3)
+;
+
+INSERT INTO btable(id, a_table_id) VALUES
+ (1, 1)
+,(2, 1)
+,(3, 1)
+,(4, 2)
+,(5, 2)
+,(6, 3)
+;
+
+INSERT INTO ctable(id, b_table_id) VALUES
+ (1, 1)
+,(2, 1)
+,(3, 1)
+,(4, 2)
+,(5, 2)
+,(6, 3)
+;
